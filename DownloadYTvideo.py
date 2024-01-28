5ca8b49864e419f997585250a6e3021af3042a14
PrBreezy DevOPS Cloud Syst Eng <pr.precival@gmail.com>
2024-01-28 23:28:47 +0800

    Update DownloadYTvideo.py

--- DownloadYTvideo.py
+++ DownloadYTvideo.py
@@ -1,5 +1,23 @@
-pip3 install pytube
+from pytube import YouTube
 
+try:
+    # Input the YouTube video link asked to user
+    link = input("Enter the YouTube URL: ")
+    
+    yt = YouTube(link)
+    
+    print("Title:", yt.title)
+    print("Views:", yt.views)
 
-import pytube 
-import sys
+    # Retrieving highest resolution available
+    yd = yt.streams.get_highest_resolution()
+    
+    # Download the video destination directory
+    yd.download()
+    
+    #When we are done showing completed message
+    print("Download complete.")
+    
+    # If any error let us know!
+except Exception as e:
+    print("An error occurred:", str(e))
